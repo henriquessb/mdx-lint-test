6ab271013cc05100700bf0a15c8444d422219b4b
Henrique Balbino <henrique.balbino@vtex.com>
2025-06-13 18:49:37 -0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,19 @@
-import * as mdxPlugin from 'eslint-plugin-mdx';
-
+import * as mdx from 'eslint-plugin-mdx'
 
 export default [
   {
-    files: ['docs/**/*.md', 'docs/**/*.mdx'],
-    plugins: { mdx: mdxPlugin },
-    languageOptions: {
-      parser: mdxPlugin.parsers['mdx'],
-    }
+    ...mdx.flat,
+    processor: mdx.createRemarkProcessor({
+      lintCodeBlocks: true,
+      languageMapper: {},
+    }),
   },
-];
+  {
+    ...mdx.flatCodeBlocks,
+    rules: {
+      ...mdx.flatCodeBlocks.rules,
+      'no-var': 'error',
+      'prefer-const': 'error',
+    },
+  },
+]
