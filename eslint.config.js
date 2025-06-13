96a1183dd65ba736c97c13e6b0603ea0ed30f782
Henrique Balbino <henrique.balbino@vtex.com>
2025-06-13 14:31:47 -0300

    Add eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import mdx from 'eslint-mdx';
+
+export default [
+  mdx(),
+  {
+    files: ['docs/**/*.md', 'docs/**/*.mdx'],
+    plugins: { mdx },
+    extends: ['plugin:mdx/recommended'],
+  },
+];
